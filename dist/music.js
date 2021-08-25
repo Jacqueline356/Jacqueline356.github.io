24431d4521c067a27fba902764f65b6eb93bb26f
Jacqueline356 <1735159307@qq.com>
2021-08-25 11:24:45 +0800

    Site updated: 2021-08-25 11:24:44

--- dist/music.js
+++ dist/music.js
@@ -6,14 +6,14 @@ const ap = new APlayer({
 	{
         name: "梅香如故",
         artist: '谭晶',
-        url: 'https://y.qq.com/n/ryqq/songDetail/004VK7Uu1yeFLv',
-        cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
+        url: 'https://www.kugou.com/song/#hash=29AF83808D63E5CC0F7E2245BE1D1133&album_id=40914405',
+        cover: 'https://imgessl.kugou.com/stdmusic/20210101/20210101203607104616.jpg',
       },
 	  {
-        name: '这些民谣 - 一次听个够',
+        name: '葡萄成熟时',
         artist: '陈奕迅',
-        url: 'https://y.qq.com/n/ryqq/songDetail/0029Zemv0kR1ur',
-        cover: 'http://p2.music.126.net/Wx5GNJEpay2JbfVUJc4Aew==/109951163094853876.jpg?param=130y130',
+        url: 'https://www.kugou.com/song/#hash=27B21D998C557F16D022EECC7DC1305C&album_id=968210',
+        cover: 'https://imgessl.kugou.com/stdmusic/20150720/20150720204638350144.jpg',
       }
     ]
 });
